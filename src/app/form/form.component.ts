c9f8cf84d6d36d5c88fe31bd85a289ec25879424
Alan Song <alansong95@gmail.com>
2020-07-21 20:39:16 -0700

    add angular material and create form component

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
